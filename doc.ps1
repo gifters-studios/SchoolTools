1c999c079dced9a0a97b80682ded464f1bd8d69d
Rayne <geichelb3512@student.pusd11.net>
2025-09-03 09:56:20 -0700

    V0.1: Code Format Change

--- doc.ps1
+++ doc.ps1
@@ -1,39 +1,30 @@
-# Load the necessary assemblies for Windows Forms
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-# Create the main form
 $Form = New-Object System.Windows.Forms.Form
 $Form.Text = "SchoolTools By Nerdiest"
 $Form.Size = New-Object System.Drawing.Size(400, 200)
 $Form.StartPosition = "CenterScreen"
-$Form.FormBorderStyle = "FixedDialog" # Prevents resizing
-$Form.MaximizeBox = $false # Disables maximize button
-$Form.MinimizeBox = $false # Disables minimize button
+$Form.FormBorderStyle = "FixedDialog"
+$Form.MaximizeBox = $false
+$Form.MinimizeBox = $false
 
-# Create a label
 $Label = New-Object System.Windows.Forms.Label
 $Label.Text = "SchoolTools"
-$Label.AutoSize = $true # Set to true initially to get the correct label width based on the text
-$Label.Font = New-Object System.Drawing.Font("Arial", 16, [System.Drawing.FontStyle]::Bold) # Use a larger font for better visual impact
+$Label.AutoSize = $true
+$Label.Font = New-Object System.Drawing.Font("Arial", 16, [System.Drawing.FontStyle]::Bold)
 
-# Calculate horizontal center for the label
-# This calculation assumes the label is parented by the form.
 $centerX = (($Form.Width - $Label.Width) / 2) - 25
 $Label.Location = New-Object System.Drawing.Point($centerX, 10)
 
 $Label2 = New-Object System.Windows.Forms.Label
 $Label2.Text = "By Nerdiest"
 $Label2.AutoSize = $true
-$Label2.Font = New-Object System.Drawing.Font("Arial", 10, [System.Drawing.FontStyle]::Italic) # Smaller font, italic style
+$Label2.Font = New-Object System.Drawing.Font("Arial", 10, [System.Drawing.FontStyle]::Italic)
 
-# Calculate horizontal center for the second label
-# It will be centered relative to the form's width, similar to the first label.
-# Position it below the first label.
 $centerX2 = (($Form.Width - $Label2.Width) / 2) + 5
-$Label2.Location = New-Object System.Drawing.Point($centerX2, ($Label1.Bottom + 35)) # Position 10 pixels below the first label
+$Label2.Location = New-Object System.Drawing.Point($centerX2, ($Label1.Bottom + 35))
 
-# Create a button
 $Button = New-Object System.Windows.Forms.Button
 $Button.Text = "Switch to Light Mode"
 $Button.Location = New-Object System.Drawing.Point(10, 120)
@@ -59,7 +50,6 @@ $Button5.Text = "Custom Wallpaper"
 $Button5.Location = New-Object System.Drawing.Point(275, 80)
 $Button5.Size = New-Object System.Drawing.Size(100, 40)
 
-# Define an action for the button click event
 $Button.Add_Click({
     Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 1 -Type Dword -Force
     Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 1 -Type Dword -Force
@@ -106,7 +96,6 @@ $Button5.Add_Click({
     [System.Windows.Forms.MessageBox]::Show("Dark mode wallpaper should be set, please wait! (Note: Your taskbar will return soon!)", "Message", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 })
 
-# Add controls to the form
 $Form.Controls.Add($Label2)
 $Form.Controls.Add($Label)
 $Form.Controls.Add($Button)
@@ -115,5 +104,4 @@ $Form.Controls.Add($Button3)
 $Form.Controls.Add($Button4)
 $Form.Controls.Add($Button5)
 
-# Display the form
 $Form.ShowDialog()
